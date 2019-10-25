90d2bd7cf121e50cf94d9f95efe3bc3f3a8f9196
Sven <sven@nimiq.com>
2019-10-25 17:36:50 +0300

    formatting code

--- src/storage/encryption/dummy.ts
+++ src/storage/encryption/dummy.ts
@@ -1,10 +1,10 @@
-import { Encryption } from "..";
+import { Encryption } from '..'
 
 export class DummyEncryption implements Encryption {
-    encrypt(data: Uint8Array): Promise<Uint8Array> {
-        return new Promise((resolve, _) => resolve(data))
-    }
-    decrypt(data: Uint8Array): Promise<Uint8Array> {
-        return new Promise((resolve, _) => resolve(data))
-    }
+  encrypt(data: Uint8Array): Promise<Uint8Array> {
+    return new Promise((resolve, _) => resolve(data))
+  }
+  decrypt(data: Uint8Array): Promise<Uint8Array> {
+    return new Promise((resolve, _) => resolve(data))
+  }
 }
