d27c5997616f022b179c50f32954c3b982ed7f92
Sven <sven@nimiq.com>
2019-10-25 18:40:23 +0300

    test that list does fail when JSON storage is corrupted

--- src/storage/index.spec.ts
+++ src/storage/index.spec.ts
@@ -10,6 +10,10 @@ const order: Order = {
 }
 
 describe('storage', () => {
+  beforeEach(function() {
+    localStorage.clear()
+  })
+
   it('encode/decode 0rder', async () => {
     const storage = new Storage('test', false, false)
 
@@ -25,9 +29,23 @@ describe('storage', () => {
     expect(localStorage.key(0).indexOf(id)).toBeGreaterThan(-1)
     expect(await storage.load(id)).toEqual(order)
 
-    const orders:Order[] = await storage.list()
+    const orders: Order[] = await storage.list()
 
     expect(orders.length == 1)
     expect(orders[0]).toEqual(order)
+  })
+
+  it('load corrupted order', async () => {
+    const storage = new Storage('test', false, false)
+
+    const id = await storage.store(order)
+
+    expect(localStorage.length == 1)
+
+    const key = localStorage.key(0)
+    // remove '[' from serialized bytes array => corrupted JSON
+    localStorage.setItem(key, localStorage.getItem(key).substr(1))
+
+    await expectAsync(storage.list()).toBeRejected()
   })
 })
