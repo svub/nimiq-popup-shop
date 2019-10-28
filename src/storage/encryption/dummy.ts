a164d0023023d2025170b8cbfae56814cee50ca2
Sven <sven@nimiq.com>
2019-10-28 10:56:11 +0200

    linting

--- src/storage/encryption/dummy.ts
+++ src/storage/encryption/dummy.ts
@@ -2,9 +2,9 @@ import { Encryption } from '..'
 
 export class DummyEncryption implements Encryption {
   encrypt(data: Uint8Array): Promise<Uint8Array> {
-    return new Promise((resolve, _) => resolve(data))
+    return new Promise(resolve => resolve(data))
   }
   decrypt(data: Uint8Array): Promise<Uint8Array> {
-    return new Promise((resolve, _) => resolve(data))
+    return new Promise(resolve => resolve(data))
   }
 }
