5daa38bba94f7df54b123b8e31f375de95737b00
Sven <sven@nimiq.com>
2019-10-28 10:48:38 +0200

    adding methods for creating shop configuration

--- src/shop.ts
+++ src/shop.ts
@@ -6,6 +6,8 @@ import {
 } from './types/shop'
 import { Storage } from './storage'
 import HubApi from '@nimiq/hub-api'
+import { WebCrypto } from './storage/encryption/webcrypto'
+import { ShopCrypto } from './types/shop'
 
 export class Shop {
   private configuration: ShopConfiguration
@@ -21,6 +23,8 @@ export class Shop {
       hubUrl || `https://hub.nimiq${live ? '' : '-testnet'}.com`,
     )
   }
+
+  // front-end
 
   async checkout(products: Product[], meta: JSON): Promise<string> {
     const price =
