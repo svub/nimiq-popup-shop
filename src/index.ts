523f98890d9b95ec8a1659c7261d92dab21bd30f
Sven <sven@nimiq.com>
2019-10-28 10:49:32 +0200

    trying to get the Shop class exported from TS to JS - not successful, using ugly hack

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,13 @@ import './polyfills'
 import './components/shop'
 import './components/checkout-button'
 import './shop'
+import { Shop } from './shop'
+
+// Shop.doSomething(Math.random())
+
+export { Shop } from './shop'
+console.log('################')
+export default Shop
+
+// only this one works so far! :(
+window['Shop'] = Shop
