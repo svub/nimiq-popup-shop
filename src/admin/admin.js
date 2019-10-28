c51bdc2539c983f8e4abb995d2ed65594ffd0626
Sven <sven@nimiq.com>
2019-10-28 10:52:41 +0200

    old admin backend - untested to be worked on next

--- src/admin/admin.js
+++ src/admin/admin.js
@@ -1,33 +1,34 @@
-import { Shop } from '../nimiq-shop.js'
-// console.log(require)
+// TODO(svub) make this proper importing work
+// import Shop from '../nimiq-shop.js'
+// import { Shop2 } from '../nimiq-shop.js'
 
 const { privateKey, publicKey } = localStorage
-const shop = new Shop()
+const shop = new Shop(configuration)
 const $ = document.getElementById.bind(document)
 
 function initialize() {
-  console.log(privateKey)
-  if (!privateKey) {
-    console.log('Setup required.')
-    return (location.href = 'setup.html')
-  }
+    console.log(privateKey)
+    if (!privateKey) {
+        console.log('Setup required.')
+        return (location.href = 'setup.html')
+    }
 
-  loadOpenOrders()
+    loadOpenOrders()
 }
 
 async function loadOpenOrders() {
-  const orders = await shop.data.list(privateKey)
-  const list = $('open_orders')
-  list.innerHTML = ''
-  if (list.length > 0) {
-    orders.forEach(order => {
-      const li = document.createElement('li')
-      li.textContent = order
-      list.appendChild(li)
-    })
-  } else {
-    list.textContent = 'none'
-  }
+    const orders = await shop.list(privateKey)
+    const list = $('open_orders')
+    list.innerHTML = ''
+    if (list.length > 0) {
+        orders.forEach(order => {
+            const li = document.createElement('li')
+            li.textContent = order
+            list.appendChild(li)
+        })
+    } else {
+        list.textContent = 'none'
+    }
 }
 
 window.addEventListener('load', initialize)
