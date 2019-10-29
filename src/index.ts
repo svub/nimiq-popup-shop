0519cc696f63eeaccdf29d5a06ad01d9b871b459
Sven <sven@nimiq.com>
2019-10-29 13:49:02 +0200

    more cleanup

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,10 @@ import './components/checkout-button'
 import './shop'
 import { Shop } from './shop'
 
-export { Shop } from './shop'
-export default Shop
+// Tried for a while to get the re-export working, but only the hack at the bottom worked so far!
+// TODO(svub) The re-export needs to be fixed. @sectore, please have a look if you can.
 
-// TODO(svub) tried for a while, but only this hack below worked so far!
-//  The re-export needs some fixing. @sectore, please have a look if you can.
+// export { Shop } from './shop'
+// export default Shop
+
 window['Shop'] = Shop
