0c7c93c307572beab9a3d82874f65f398fb655c5
Sven <sven@nimiq.com>
2019-11-04 13:05:08 +0200

    splitting Backend > Shop, Frontend > Shop: rename admin to backend

--- src/backend/backend.ts
+++ src/backend/backend.ts
@@ -1,10 +1,29 @@
 import { Shop } from '../shop'
 import { ShopConfiguration, ShopCrypto, Order } from '../types/shop'
 import { WebCrypto } from '../storage/encryption/webcrypto'
+import Nimiq, { Client } from '@nimiq/core-web'
+
+const decoder: TextDecoder = new TextDecoder()
 
 export class Backend extends Shop {
   async list(privateKey: JsonWebKey): Promise<Order[]> {
     return this.storage.list(privateKey)
+  }
+
+  async bs(privateKey: JsonWebKey): Promise<void> {
+    if (this.configuration.live) Nimiq.GenesisConfig.main()
+    else {
+      Nimiq.GenesisConfig.test()
+    }
+    const nimiq = Nimiq.Client.Configuration.builder().instantiateClient()
+    nimiq.addTransactionListener(
+      async (transaction: Client.TransactionDetails) => {
+        const orderId = decoder.decode(transaction.data.raw)
+        const order = await super.storage.load(orderId, privateKey)
+        const correct = order.products.map(product => product.price).
+      },
+      [this.configuration.address],
+    )
   }
 
   static async generateCrypto(): Promise<ShopCrypto> {
