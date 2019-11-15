cdc6e891d30cab28fd671709dc5e3d2236092517
Sven <sven@nimiq.com>
2019-11-15 16:06:14 -0600

    minimal shop component

--- src/components/shop.ts
+++ src/components/shop.ts
@@ -6,6 +6,8 @@ import {
   TemplateResult,
 } from 'lit-element'
 import { TAG_NAME as BUTTON_TAG_NAME } from './checkout-button'
+import { Frontend } from '../frontend'
+import { Product } from '../types/shop'
 
 const TAG_NAME = 'nimiq-shop'
 
@@ -20,12 +22,51 @@ const TAG_NAME = 'nimiq-shop'
 @customElement(TAG_NAME)
 export class NimiqShop extends LitElement {
   @property({ type: String })
-  address = ''
+  config
+
+  @property({ type: String })
+  collectMetadata
+
+  @property({ type: String })
+  onError
+
+  private metadataCallback: Function = product => {
+    console.warn('collect metadata not implements')
+    console.log(product)
+  }
+
+  private errorCallback: Function = error => {
+    console.warn(error)
+  }
+
+  private frontend: Promise<Frontend>
+
+  constructor() {
+    super()
+    this.config = 'nimiq-pop-up-shop-configuration.json'
+    this.checkout = this.checkout.bind(this)
+  }
+
+  firstUpdated(changedProperties): void {
+    console.log(this.config)
+    this.frontend = new Promise<Frontend>(resolve => {
+      fetch(location + this.config)
+        .then(data => data.json())
+        .then(configuration => resolve(new Frontend(configuration)))
+    })
+  }
 
   connectedCallback(): void {
     super.connectedCallback()
     for (const button of document.getElementsByTagName(BUTTON_TAG_NAME)) {
       button.addEventListener('checkout', this.checkout)
+    }
+
+    if (this.collectMetadata) {
+      this.metadataCallback = eval(this.collectMetadata)
+    }
+    if (this.onError) {
+      this.errorCallback = eval(this.onError)
     }
   }
 
@@ -36,8 +77,19 @@ export class NimiqShop extends LitElement {
     }
   }
 
-  protected checkout(ev: CustomEvent): void {
+  protected async checkout(ev: CustomEvent): Promise<string> {
     console.log('checkout', ev)
+    const frontend = await this.frontend
+    const products: Product[] = [ev.detail]
+    try {
+      const metadata = await this.metadataCallback(
+        products,
+        frontend.sumUp(products),
+      )
+      if (metadata) return frontend.checkout(products, metadata)
+    } catch (e) {
+      this.errorCallback(e)
+    }
   }
 
   render(): TemplateResult {
