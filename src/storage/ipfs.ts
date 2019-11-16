0484823d5d523b5d2480ce86dc73507b9ab66f1a
Sven <sven@nimiq.com>
2019-11-16 15:17:43 -0600

    adding ipfs, impl store and load

--- src/storage/ipfs.ts
+++ src/storage/ipfs.ts
@@ -1,30 +1,30 @@
-import { Order } from '../types/shop'
 import { StorageBackend } from './index'
+import { Buffer } from 'buffer'
+import Ipfs from 'ipfs'
 
 export class IpfsStorage implements StorageBackend {
   protected repository: string
-  protected ipfs
+  protected ipfs = Ipfs.createNode(null)
 
   constructor(repository: string) {
     this.repository = repository
-    console.warn('not implemented!')
-    // TODO(svub): load IPFS
-    this.ipfs = repository
   }
 
-  store(data: Uint8Array): Promise<string> {
-    console.warn('not implemented!')
-    // TODO(sectore): Implementation
-    return null
+  async store(data: Uint8Array): Promise<string> {
+    // @ts-ignore seems types are not up-to-date with the JS
+    const orderId = (await this.ipfs.add(Buffer.from(data)))[0].hash
+    return orderId
   }
 
-  load(id: string): Promise<Uint8Array> {
-    console.warn('not implemented!')
-    // TODO(sectore): Implementation
-    return null
+  async load(id: string): Promise<Uint8Array> {
+    console.log(`IPFS online ${this.ipfs.isOnline()}`)
+    // @ts-ignore seems types are not up-to-date with the JS
+    const buffer: Buffer = await this.ipfs.cat(id)
+    console.log(buffer.toString('utf8'))
+    return new Uint8Array(buffer)
   }
 
-  list(): Promise<Uint8Array[]> {
+  async list(): Promise<Uint8Array[]> {
     console.warn('not implemented!')
     // TODO(sectore): Implementation
     return null
